1ac87aa5c4e1ac20a280e0d221182ea476ca5335
anti256 <anti256@mail.ru>
2023-10-10 17:11:58 +0300

    Tic-Tac_Toe start coding

--- src/main/java/com/example/tictactoe/HelloApplication.java
+++ src/main/java/com/example/tictactoe/HelloApplication.java
@@ -3,11 +3,15 @@ package com.example.tictactoe;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -15,9 +19,15 @@ public class HelloApplication extends Application {
         stage.setTitle("Крестики-нолики");
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
         launch();
     }
 }
+
+//alignment="CENTER" contentDisplay="CENTER" mnemonicParsing="false" prefHeight="23.0" prefWidth="40.0" text="X" textAlignment="CENTER" GridPane.columnIndex="3">
+//               <font>
+//                  <Font name="Arial Narrow" size="14.0" />
+//               </font>
--- src/main/java/com/example/tictactoe/HelloController.java
+++ src/main/java/com/example/tictactoe/HelloController.java
@@ -1,14 +1,41 @@
 package com.example.tictactoe;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.TextAlignment;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    private GridPane grid;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    @FXML
+    void initialize(){
+        Button btn = new Button();
+        btn.setText("u");
+        btn.setAlignment(Pos.CENTER);
+        btn.setContentDisplay(ContentDisplay.CENTER);
+        btn.setPrefSize(40.0,40.0);
+        btn.setMnemonicParsing(false);
+        btn.setTextAlignment(TextAlignment.CENTER);
+
+        grid.add(btn);
+
+    }
 }
+
+<Button
+        GridPane.columnIndex="3">
+<font>
+<Font name="Arial Narrow" size="14.0" />
+</font>
+</Button>
