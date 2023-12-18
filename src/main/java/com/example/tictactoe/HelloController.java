eaf9bd9539f84f4430ba26d25ae9ec8c41e45617
anti256 <anti256@mail.ru>
2023-12-18 17:26:19 +0300

    codding 18_12_23

--- src/main/java/com/example/tictactoe/HelloController.java
+++ src/main/java/com/example/tictactoe/HelloController.java
@@ -28,16 +28,19 @@ public class HelloController {
     @FXML
     private AnchorPane mainWindow;
     @FXML
-    ArrayList<ButtonClass> buttonArray = new ArrayList<>();
+    ArrayList<ButtonClass> buttonArray = new ArrayList<>();//список всех кнопок
     @FXML
     ArrayList<ButtonClass> compPoints = new ArrayList<>();//клетки компьютера
     @FXML
     ArrayList<ButtonClass> userPoints = new ArrayList<>();//клетки человека
+    @FXML
+    ArrayList<ButtonClass> possiblePoints = new ArrayList<>();//клетки, доступные для нажатия
 
     //HashMap<Integer, Integer> compPoints = new HashMap<>();
     //HashMap<Integer, Integer> userPoints = new HashMap<>();
     //ArrayList<Points> userPoints = new ArrayList<>();
-    String whoseMove;
+    @FXML
+    String whoseMove;//чей ход - human или computer
 
     @FXML
     void btnClick(ActionEvent event) {
@@ -63,6 +66,7 @@ public class HelloController {
 
     @FXML
     void initialize(){
+        whoseMove = (((int) (100 * Math.random())) < 50) ? "human" : "computer";//100 * (int) Math.random()) < 50
         for (int i = 0; i < 25; i++) {
             for (int j = 0; j < 25; j++) {
                 Button button = new Button(" ");
@@ -75,18 +79,23 @@ public class HelloController {
 //                button.setTextAlignment(TextAlignment.CENTER);
 //                button.setFont(Font.font("Arial Narrow", 14.0));
                 button.setOnAction(this::btnClick);
-                grid.add(button, i, j);
+                grid.add(button, j, i);
                 //button.setVisible(true);
-                ButtonClass btnCl = new ButtonClass(button, i, j);
+                ButtonClass btnCl = new ButtonClass(button, j, i);
                 buttonArray.add(btnCl);
                 //btnCl.getButton(i, j).setVisible(true);
             }
         }
+        /*for (ButtonClass bc: buttonArray
+             ) {
+            bc.getButton().setText(buttonArray.indexOf(bc) + "");
+        }*/
         //grid.setVisible(true);
     }//end of initialize()
 
     @FXML
     void newGame(ActionEvent event) {
+        whoseMove = (whoseMove.equals("human")) ? "computer": "human";//перещелкиваем игрока
         //очищаем игровое поле
         for (ButtonClass btnC: buttonArray
              ) {
@@ -94,15 +103,45 @@ public class HelloController {
         }
         userPoints.clear();
         compPoints.clear();
-        buttonArray.stream()
+        possiblePoints.clear();
+        possiblePoints.add(buttonArray.get(312));
+        possiblePoints.add(buttonArray.get(313));
+        possiblePoints.add(buttonArray.get(314));
+        possiblePoints.add(buttonArray.get(337));
+        possiblePoints.add(buttonArray.get(339));
+        possiblePoints.add(buttonArray.get(362));
+        possiblePoints.add(buttonArray.get(363));
+        possiblePoints.add(buttonArray.get(364));
+        for (ButtonClass b: possiblePoints
+             ) {
+            b.getButton().setText("A");
+        }
+
+       if (whoseMove.equals("human")){
+           buttonArray.get(338).getButton().setStyle("-fx-text-fill: #0000ff");
+           buttonArray.get(338).setText("X");
+           int cCurr = (int) ((possiblePoints.size() - 1) * Math.random());
+           possiblePoints.get(cCurr).getButton().setStyle("-fx-text-fill: #ff0000");
+           possiblePoints.get(cCurr).getButton().setText("O");
+           possiblePoints.remove(cCurr);
+           searchEmptyButton(buttonArray.indexOf(possiblePoints.get(cCurr)));
+           whoseMove = "computer";
+        } else {
+           buttonArray.get(338).getButton().setStyle("-fx-text-fill: #ff0000");
+           buttonArray.get(338).setText("O");
+           //possiblePoints.clear();
+        }
+       /* buttonArray.stream()
             .filter(ButtonC -> ButtonC.getButton().equals(ButtonC.getButton(13, 13)))
             .forEach(x -> x.setText("X"));
 
+        buttonArray.get(338).setText("O");*/
+
 
     //compPoints.add(new Points(13, 13));
-    whoseMove = "human";
+    /*whoseMove = "human";
     buttonArray.get(0).getButton().setVisible(true);
-    buttonArray.get(0).setText("sdf");
+    buttonArray.get(0).setText("sdf");*/
     }
 
     ButtonClass buttonClassByButton (Button btn){
@@ -111,6 +150,16 @@ public class HelloController {
             if (bc.getButton().equals(btn)) {return bc;}
         }
         return  null;
+    }
+
+    //поиск пустых кнопок около отмеченной и добавление их в список которых можно кликнуть
+    void searchEmptyButton (int indeX){
+        int [] ints = new int[] {indeX - 26, indeX - 25, indeX - 24, indeX - 1, indeX +1, indeX + 24, indeX + 25, indeX + 26};
+        for (int i = 0; i < ints.length; i++) {
+            if (!buttonArray.get(ints[i]).getButton().getText().equals("")) {
+                possiblePoints.add(buttonArray.get(ints[i]));
+            }
+        }
     }
 
 }
